186e867c0f21c00308695773958bc6f55b8a80c4
Vasya <inqire@gmail.com>
2018-05-04 12:25:59 +0300

    Add second implementation in task 4.

---
+++ Task4.Solution/SecondWay/CalculatorWithDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.Solution.SecondWay
+{
+    public delegate double CalcDelegate(List<double> value);
+
+    public class CalculatorWithDelegate
+    {
+
+        public double CalculateAverage(List<double> values, CalcDelegate calcDelegate)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (calcDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(calcDelegate));
+            }
+
+            return calcDelegate(values);
+        }
+    }
+}
--- Task4.Tests/TestCalculator.cs
+++ Task4.Tests/TestCalculator.cs
@@ -2,9 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using NUnit.Framework;
     using Task4.Solution;
     using Task4.Solution.AveregeMethod;
+    using Task4.Solution.SecondWay;
 
     [TestFixture]
     public class TestCalculator
@@ -19,6 +21,28 @@
             double expected = 8.3636363;
 
             double actual = calculator.CalculateAverage(values, new AveregeMean());
+
+            Assert.AreEqual(expected, actual, 0.000001);
+        }
+
+        [Test]
+        public void Test_AverageByMean_delegate()
+        {
+            var calculator = new CalculatorWithDelegate();
+
+            double expected = 8.3636363;
+
+            CalcDelegate calcDelegate = delegate (List<double> values)
+            {
+                if (values == null)
+                {
+                    throw new ArgumentNullException(nameof(values));
+                }
+                return values.Sum() / values.Count;
+            };
+
+            var actual = calculator.CalculateAverage(values, calcDelegate);
+            
 
             Assert.AreEqual(expected, actual, 0.000001);
         }
